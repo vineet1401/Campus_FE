3c56ec4e0a36f10b0f9f085b035208a85917f121
Akshayraut33 <akshayraut03052003@gamil.com>
2025-04-11 20:36:41 +0530

    Student list table ui

--- src/components/StudentsTabel/DebouncedInput.js
+++ src/components/StudentsTabel/DebouncedInput.js
@@ -1,36 +1,6 @@
 import { useEffect, useState } from "react";
 
 const DebouncedInput = ({
-<<<<<<< HEAD
-  value: initValue,
-  onChange,
-  debounce = 500,
-  ...props
-}) => {
-  const [value, setValue] = useState(initValue);
-
-  useEffect(() => {
-    setValue(initValue);
-  }, [initValue]);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      onChange(value);
-    }, debounce);
-    return () => clearTimeout(timeout);
-  }, [value]);
-
-  return (
-    <input
-      {...props}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-    />
-  );
-};
-
-export default DebouncedInput;
-=======
     value: initValue,
     onChange,
     debounce = 500,
@@ -59,4 +29,3 @@ export default DebouncedInput;
 };
 
 export default DebouncedInput;
->>>>>>> 5b22f305c56345030f8fd92dc74e96b033af46f4
