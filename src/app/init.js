21f6beb0c1250a394f14ccee5034551fab399b92
vineet-1401 <vineetsakpal.work@gmail.com>
2025-04-13 22:00:40 +0530

    Cloud DEPLOYMENT 1.0

--- src/app/init.js
+++ src/app/init.js
@@ -3,8 +3,8 @@ import axios from "axios"
 const initializeApp = () => {
     
     // Setting base URL for all API request via axios
-    axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_LOCAL
-    // axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_CLOUD
+    // axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_LOCAL
+    axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_CLOUD
 
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
         // dev code
