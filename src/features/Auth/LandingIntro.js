0e5579413727dd87efbed8dd908bb31e59580e76
om <patilomraje590@gmail.com>
2025-06-13 09:16:22 +0530

    add login page image

--- src/features/Auth/LandingIntro.js
+++ src/features/Auth/LandingIntro.js
@@ -1,28 +1,28 @@
 
 function LandingIntro() {
   return (
-    <div className="hero min-h-full rounded-l-xl bg-base-200">
-      <div className="hero-content py-12">
-        <div className="max-w-md">
-          <h1 className="text-3xl text-center font-bold ">
+    <div className="hero h-full w-full rounded-l-xl bg-base-200 ">
+      {/* <div className="hero-content py-8">
+        <div className="max-w-md"> */}
+      {/* <h1 className="text-3xl text-center font-bold ">
             <img
               src="/logo192.png"
               className="w-12 inline-block mr-2 mask mask-circle"
               alt="dashwind-logo"
             />
             DashWind
-          </h1>
+          </h1> */}
 
-          <div className="text-center mt-12">
-            <img
-              src="./intro.png"
-              alt="Dashwind Admin Template"
-              className="w-48 inline-block"
-            ></img>
-          </div>
-
-        </div>
+      <div className="text-center ">
+        <img
+          src="./Campus Connect.png"
+          alt="Dashwind Admin Template"
+          className="w-80 inline-block h-80"
+        ></img>
       </div>
+
+      {/* </div>
+      </div> */}
     </div>
   );
 }
